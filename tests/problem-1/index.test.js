04e7da56cf865f256910a6a1d3ae2c2f41da4115
Mahendran GK <gkm09@hotmail.com>
2018-02-06 00:01:38 +0530

    Updated the test description

--- tests/problem-1/index.test.js
+++ tests/problem-1/index.test.js
@@ -2,12 +2,12 @@ import test from 'ava';
 
 import { getSum } from "../../src/problem-1/index.js";
 
-test("Get sum of multiples of 3 or 5 between 10", t => {
+test("1 - Get sum of multiples of 3 or 5 between 10", t => {
     let sum = getSum(10);
     t.is(sum,23);
 });
 
-test("Get sum of multiples of 3 or 5 between 1000", t => {
+test("1 - Get sum of multiples of 3 or 5 between 1000", t => {
   let sum = getSum(1000);
   t.is(sum, 233168);
 });
