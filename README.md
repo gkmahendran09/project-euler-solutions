6a5aa308e889c41c78f9bcf9e9e119a15e252fd2
Mahendran GK <gkm09@hotmail.com>
2018-02-05 21:55:10 +0530

    Problem 1 solved.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project-euler-solutions
+# Project Euler Solutions
---
+++ problem-1/index.js
@@ -0,0 +1,14 @@
+function getSum(limit) {
+  var sum = 0;
+
+  for (let i = 0; i < limit; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+
+getSum(1000);
+// Ans - 233168
