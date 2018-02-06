a5a1e8e2cc6aa783045b30cffb935dc97ca1d021
Mahendran GK <gkm09@hotmail.com>
2018-02-07 00:03:10 +0530

    Added additional test input for each tests

--- test.js
+++ test.js
@@ -4,6 +4,10 @@ import { getSum } from "./src/1.js";
 test("1 - Get sum of multiples of 3 or 5 between 10", t => {
   let sum = getSum(10);
   t.is(sum, 23);
+  // 10 - 23
+
+  let sum1 = getSum(1000);
+  t.is(sum1, 233168);
   // 1000 - 233168
 });
 
@@ -11,16 +15,24 @@ import { getSum as getSum2 } from "./src/2.js";
 test("2 - Get sum of even Fibonacci numbers between 10", t => {
   let sum = getSum2(10);
   t.is(sum, 10);
+  // 10 - 10
+
+  let sum1 = getSum2(4000000);
+  t.is(sum1, 4613732);
   // 4000000 - 4613732
 });
 
 import { getLargestPrime } from "./src/3.js";
 test("3 - Get the largest prime factors of 13195", t => {
   // console.time("getLargestPrime");
-  let largestPrime = getLargestPrime(600851475143);
+  let largestPrime = getLargestPrime(13195);
   // console.timeEnd("getLargestPrime");
-  t.is(largestPrime, 6857);
+  t.is(largestPrime, 29);
   // 13195 - 29
+
+  let largestPrime1 = getLargestPrime(600851475143);
+  // console.timeEnd("getLargestPrime");
+  t.is(largestPrime1, 6857);
   // 600851475143 - 6857
 });
 
